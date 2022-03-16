35e4193ed1ebf0ad73555c35d020fc1b46e91d60
Paulo Juliani <paulojuliani@hotmail.com>
2022-03-16 15:40:50 -0300

    adicionado o arquivo index.html e pasta src

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+
+    pokemon.addEventListener('click', () => {
+
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const idDoCartaoParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaLista = document.querySelector('.ativo')
+        pokemonAtivoNaLista.classList.remove('ativo')
+
+        const pokemonSelecionadoNaLista = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaLista.classList.add('ativo')
+    })
+})
